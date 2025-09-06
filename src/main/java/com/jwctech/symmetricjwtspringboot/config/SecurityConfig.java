c418038eec8cbbf0414c64bda387d35b09047d40
James Wyatt <jamesrwyatt35@gmail.com>
2025-09-05 20:59:44 -0400

    security config allow home page

---
+++ src/main/java/com/jwctech/symmetricjwtspringboot/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.jwctech.symmetricjwtspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        //Allow all request for home page with permit all
+                        .requestMatchers("/").permitAll()
+                        //AnyRequest is a catch-all for any request that doesn't match the above
+                        .anyRequest().authenticated()
+                )
+                //Set the session management to stateless
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
