c21fa22a0ee21a483f5dc687f98f55a69ef44e14
James Wyatt <jamesrwyatt35@gmail.com>
2025-09-05 20:24:05 -0400

    Initial commit

---
+++ src/main/java/com/jwctech/symmetricjwtspringboot/SymmetricJwtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jwctech.symmetricjwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymmetricJwtSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymmetricJwtSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwctech/symmetricjwtspringboot/SymmetricJwtSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwctech.symmetricjwtspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymmetricJwtSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
