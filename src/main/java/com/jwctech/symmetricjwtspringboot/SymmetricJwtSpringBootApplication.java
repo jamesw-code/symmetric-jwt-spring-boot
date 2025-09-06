5441fdb75bb9d706b159ca38decaf560b932a867
James Wyatt <jamesrwyatt35@gmail.com>
2025-09-05 20:24:38 -0400

    init commit

--- src/main/java/com/jwctech/symmetricjwtspringboot/SymmetricJwtSpringBootApplication.java
+++ src/main/java/com/jwctech/symmetricjwtspringboot/SymmetricJwtSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SymmetricJwtSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SymmetricJwtSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SymmetricJwtSpringBootApplication.class, args);
+    }
 
 }
